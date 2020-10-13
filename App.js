3861f8aee1a78a07e9d6d7bec65895ab0412424c
Ian <iangu234@gmail.com>
2020-10-13 14:37:32 -0400

    COD-24 fix import issues

--- App.js
+++ App.js
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
+
 import {
   SafeAreaView,
   StyleSheet,
@@ -23,10 +16,10 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
 import {
-  CMTextfield,
-  CMButton
-} from './components/index.js';
+  CMTextfield
+} from './src/components/index.js';
 
 const App: () => React$Node = () => {
   return (
