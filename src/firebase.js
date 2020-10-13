20e45efad33c853ac10c6083050d95caea2201fe
Ian <iangu234@gmail.com>
2020-10-13 14:39:20 -0400

    COD-30 setup firebase integration (auth, firestore, storage)

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
+
+function auth() {
+    return auth();
+}
+
+function firestore() {
+    return firestore();
+}
+
+function storage() {
+    return storage();
+}
+
