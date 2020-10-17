2ed09127bb682cb84cb1b21cd4c0fe10acbc2fa4
Ian <iangu234@gmail.com>
2020-10-16 23:49:13 -0400

    COD-31 fix multidex bug and navigator rendering bug

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import {default as Colors, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import DisplayExample from "./src/controller/ExampleController";
@@ -9,11 +10,13 @@ const Stack = createStackNavigator();
 const App: () => React$Node = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Example" component={DisplayExample} options={{ title: 'Welcome' }}/> {/* component should be contained in respective controller */}
-                {/*<Stack.Screen name="Profile" component={ProfileScreen} />*/}
+            <Stack.Navigator initialRouteName={"Example"}>
+                <Stack.Screen name="Example" component={DisplayExample}/>
             </Stack.Navigator>
         </NavigationContainer>
+        /* component should be contained in respective controller */
+        /*<Stack.Screen name="Profile" component={ProfileScreen} />*/
+        // IMPORTANT do not put comments / extra spaces inside the Stack.Navigator tab. It's super strict and will not render.
     );
 };
 
