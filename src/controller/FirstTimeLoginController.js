fd7f620bcbdb9fadad79960af99f7c10916d5e8a
Kevin Oh <kevoh1516@gmail.com>
2020-10-20 11:54:15 -0400

    create questionnaire

--- src/controller/FirstTimeLoginController.js
+++ src/controller/FirstTimeLoginController.js
@@ -1,42 +1,41 @@
 import UserModel from '../model/UserModel';
-import {useContext} from "react";
-import {AuthContext} from "../navigation/AuthNavigator";
-import {Loading} from "../view/firstTimeLogin/Buffer";
-import {QuestionnaireView} from "../view/firstTimeLogin/QuestionnaireView";
-import {ProfileSetupView} from "../view/firstTimeLogin/ProfileSetupView";
-
+import {useContext} from 'react';
+import {AuthContext} from '../navigation/AuthNavigator';
+import {Loading} from '../view/firstTimeLogin/Buffer';
+import {QuestionnaireView} from '../view/firstTimeLogin/QuestionnaireView';
+import {ProfileSetupView} from '../view/firstTimeLogin/ProfileSetupView';
 
 export const StartupCheck = ({route, navigation}) => {
-    const user = useContext(AuthContext);
-    // UserModel.signOut();
-    check = UserModel.firstTimeLoginChecks(user.uid);
-    // if (check === 2)
-        navigation.navigate('Questionnaire');
-    // else if (check === 1)
-    //     navigation.navigate('ProfileSetup');
-    // else
-    //     navigation.navigate('ProfileView');
+  const user = useContext(AuthContext);
+  // UserModel.signOut();
+  check = UserModel.firstTimeLoginChecks(user.uid);
+  // if (check === 2)
+  navigation.navigate('Questionnaire');
+  // else if (check === 1)
+  //     navigation.navigate('ProfileSetup');
+  // else
+  //     navigation.navigate('ProfileView');
 
-    return Loading();
+  return Loading();
 };
 
 export const Questionnaire = ({route, navigation}) => {
-    const user = useContext(AuthContext);
+  const user = useContext(AuthContext);
 
-    function onSubmit(responses) {
-        const {q1, q2, q3, q4, q5, q6} = responses;
-        UserModel.updateQuestionnaire(user.uid, responses);
-        navigation.navigate('ProfileSetup');
-    }
+  function onSubmit(responses) {
+    const {q1, q2, q3, q4, q5, q6} = responses;
+    UserModel.updateQuestionnaire(user.uid, responses);
+    navigation.navigate('ProfileSetup');
+  }
 
-    return QuestionnaireView({});
+  return QuestionnaireView({onSubmit});
 };
 
 export const ProfileSetup = ({route, navigation}) => {
-    function onSubmit(profileObj) {
-        UserModel.updateProfile(profileObj);
-        navigation.navigate('ProfileView');
-    }
+  function onSubmit(profileObj) {
+    UserModel.updateProfile(profileObj);
+    navigation.navigate('ProfileView');
+  }
 
-    return ProfileSetupView({onSubmit});
+  return ProfileSetupView({onSubmit});
 };
--- src/view/firstTimeLogin/QuestionnaireView.js
+++ src/view/firstTimeLogin/QuestionnaireView.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import Text from "react-native-paper/src/components/Typography/Text";
+import {View} from 'react-native';
+import {Text, TextInput, Button} from 'react-native-paper';
 
 export function QuestionnaireView(props) {
-    return (
-      <>
-          <Text>
-              questionnaire
-          </Text>
-      </>
-    );
+  const [q1, setQ1] = React.useState('');
+  const [q2, setQ2] = React.useState('');
+  const [q3, setQ3] = React.useState('');
+  const [q4, setQ4] = React.useState('');
+  const [q5, setQ5] = React.useState('');
+
+  return (
+    <View style={{padding: 50}}>
+      <Text>Favorite sport?</Text>
+      <TextInput
+        style={{marginBottom: 20}}
+        placeholder="Question 1"
+        onChangeText={(text) => setQ1(text)}
+      />
+      <Text>Age</Text>
+      <TextInput
+        style={{marginBottom: 20}}
+        placeholder="Question 2"
+        onChangeText={(text) => setQ2(text)}
+      />
+      <Text>Highest level of sport play?</Text>
+      <TextInput
+        style={{marginBottom: 20}}
+        placeholder="Question 3"
+        onChangeText={(text) => setQ3(text)}
+      />
+      <Text>What sport would you like to learn / know about?</Text>
+      <TextInput
+        style={{marginBottom: 20}}
+        placeholder="Question 4"
+        onChangeText={(text) => setQ4(text)}
+      />
+      <Text>Favorite sports team?</Text>
+      <TextInput
+        style={{marginBottom: 20}}
+        placeholder="Question 5"
+        onChangeText={(text) => setQ5(text)}
+      />
+      <Button
+        mode="contained"
+        onPress={() => props.onSubmit({q1, q2, q3, q4, q5})}>
+        Submit
+      </Button>
+    </View>
+  );
 }
