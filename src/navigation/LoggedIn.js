4b83ec6e2a5dda3f5bc716d1821152e1392ac006
victguo <victorguo_123@outlook.com>
2020-10-28 19:06:03 -0400

    COD-32 Added bottom nav bar into the UI of a fully signed-up user

--- src/navigation/LoggedIn.js
+++ src/navigation/LoggedIn.js
@@ -2,9 +2,48 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import { Profile } from '../controller/ProfileController';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import {ProfileSetup, Questionnaire, StartupCheck, TheZone} from "../controller/FirstTimeLoginController";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+// TODO: ADD LIVEVIEW AND SETTINGS VIEW INTO TAB
+
+function FullySignedUp() {
+  return (
+    <Tab.Navigator initialRouteName={"The Zone"}>
+      <Tab.Screen name="The Zone" component={TheZone} options={{
+          tabBarLabel: "The Zone",
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="stadium-variant" color="#900" size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen name="Profile" component={Profile} options={{
+           tabBarLabel: "Profile",
+           tabBarIcon: () => (
+             <MaterialCommunityIcons name="face-profile" color="#900" size={30} />
+           ),
+         }}
+      />
+      <Tab.Screen name="Settings" component={TheZone} options={{
+          tabBarLabel: "Settings",
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="cog" color="#900" size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen name="Live" component={TheZone} options={{
+          tabBarLabel: "Live",
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="youtube-tv" color="#900" size={30} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function LoggedInStack() {
   return (
@@ -13,8 +52,7 @@ export default function LoggedInStack() {
         <Stack.Screen name="StartupCheck" component={StartupCheck} />
         <Stack.Screen name="Questionnaire" component={Questionnaire} />
         <Stack.Screen name="ProfileSetup" component={ProfileSetup} />
-        <Stack.Screen name="ProfileView" component={Profile} />
-        <Stack.Screen name="TheZoneView" component={TheZone} />
+        <Stack.Screen name="TheZoneView" component={FullySignedUp} />
       </Stack.Navigator>
     </NavigationContainer>
   )
