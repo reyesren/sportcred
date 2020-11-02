a1fd8188002790e9e793a9a9c2d9656b92ad8b42
FarhanChowdhury248 <farhanChowdhury248@users.noreply.github.com>
2020-11-02 03:13:53 -0500

    COD-55 Added front-end code for trivia challenge notifs

--- src/controller/TriviaController.js
+++ src/controller/TriviaController.js
@@ -1,10 +1,9 @@
 import TriviaLandingView from '../view/trivia/TriviaLandingView';
 import {useContext} from 'react';
 import {AuthContext} from '../navigation/AuthNavigator';
+import {TriviaChallenge} from '../model/TriviaChallengeModel';
 
 export const TriviaLanding = ({route, navigation}) => {
-  const user = useContext(AuthContext);
-
   const {msg} = route.params === undefined ? {} : route.params;
 
   const goToTriviaStartGame = () => {
@@ -13,3 +12,10 @@ export const TriviaLanding = ({route, navigation}) => {
 
   return TriviaLandingView({goToTriviaStartGame});
 };
+
+export function getChallenges() {
+  const user = useContext(AuthContext);
+  const challenges = TriviaChallenge.getChallenges(user.uid);
+  console.log(challenges);
+  return challenges;
+}
--- src/view/trivia/TriviaLandingView.js
+++ src/view/trivia/TriviaLandingView.js
@@ -2,8 +2,60 @@ import React from 'react';
 import {View, Image, StyleSheet} from 'react-native';
 import Text from 'react-native-paper/src/components/Typography/Text';
 import {Button, TextInput, Title} from 'react-native-paper';
+import {getChallenges} from './../../controller/TriviaController';
+
+const ChallengeNotification = (props) => {
+  const styles = StyleSheet.create({
+    notificationContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      margin: 20,
+      paddingTop: 20,
+      borderTopWidth: 3
+    },
+    challengeTextContainer: {
+      flex: 2,
+      flexDirection:'column'
+    },
+    challengeText: {
+      fontSize: 17
+    },
+    buttonContainer: {
+      flex:1,
+      flexDirection: 'column'
+    }
+  });
+
+  return (
+    <>
+    <View style={styles.notificationContainer}>
+      <View style={styles.challengeTextContainer}>
+        <Text style={styles.challengeText}>
+          {props.challengerId} is challenging you to a trivia contest!
+        </Text>
+      </View>
+      <View style={styles.buttonContainer}>
+          <Button mode="contained" onPress={() => {}}>Accept</Button>
+          <Button mode="contained" onPress={() => {}}>Decline</Button>
+      </View>
+    </View>
+    </>
+  );
+}
+
 
 const TriviaLandingView = (props) => {
+  //const challenges = getChallenges();
+  const challenges = [{challengerId: 'challenger1', time: '82358267'}];
+
+  function renderChallengeNotifications() {
+    return challenges.map((challenge) => {
+      return <ChallengeNotification 
+              challengerId={challenge.challengerId} 
+              key={challenge.challengerId} />;
+      });
+  }
+
   return (
     <View>
       <Image
@@ -23,6 +75,7 @@ const TriviaLandingView = (props) => {
         onPress={() => props.goToTriviaStartGame()}>
         SOLO
       </Button>
+      {renderChallengeNotifications()}
     </View>
   );
 };
@@ -31,8 +84,8 @@ const styles = StyleSheet.create({
   button: {
     marginLeft: 20,
     marginRight: 20,
-    marginTop: 40,
-    marginBottom: 40,
+    marginTop: 20,
+    marginBottom: 20,
   },
   logo: {
     width: '100%',
