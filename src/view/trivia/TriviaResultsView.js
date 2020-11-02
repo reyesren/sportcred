c4a002f2603c0865bc86d579a91ebec6a7493ede
Kevin Oh <kevoh1516@gmail.com>
2020-11-01 19:37:29 -0500

    [COD-38] Results UI no logic yet

--- src/view/trivia/TriviaResultsView.js
+++ src/view/trivia/TriviaResultsView.js
@@ -1,13 +1,48 @@
-import {View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {StyleSheet, View} from 'react-native';
+import {Divider, Text, Title} from 'react-native-paper';
 import React from 'react';
 
 const TriviaResultView = () => {
   return (
-    <View>
-      <Text>Trivia results screen</Text>
+    <View style={styles.container}>
+      <Title style={styles.header}>TRIVIA</Title>
+      <Text style={styles.subheader}>Results</Text>
+      <Text style={styles.questions}>
+        You got <Text style={styles.score}>7</Text> questions right!
+      </Text>
+      <Text style={styles.ACS}>ACS: ? + ?</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    padding: 35,
+  },
+  header: {
+    alignSelf: 'center',
+    paddingBottom: 50,
+  },
+  subheader: {
+    fontSize: 20,
+    paddingBottom: 30,
+    color: 'grey',
+  },
+  questions: {
+    fontSize: 25,
+    paddingBottom: 30,
+    alignSelf: 'center',
+  },
+  score: {
+    color: 'green',
+  },
+  ACS: {
+    fontSize: 25,
+    paddingBottom: 30,
+    alignSelf: 'center',
+  },
+});
 
 export default TriviaResultView;
