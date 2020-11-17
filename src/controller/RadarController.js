ee077d2c6b847fb43050e566a9fc5f30bc0309ac
FarhanChowdhury248 <farhanChowdhury248@users.noreply.github.com>
2020-11-16 19:56:41 -0500

    Added components for radar view

--- src/controller/RadarController.js
+++ src/controller/RadarController.js
@@ -1,7 +1,29 @@
 import React from "react";
 import {RadarView} from "./../view/RadarView";
+import UserModel from './../model/UserModel';
+import {useContext} from 'react';
+import {AuthContext} from '../navigation/AuthNavigator';
 
 export const Radar = () => {
 
     return RadarView();
+}
+
+export function getRadarList() {
+    const user = useContext(AuthContext);
+    var idList = UserModel.userDocObj.radar_list;
+    
+
+    console.log('ID: ');
+    console.log(UserModel.userDocObj.radar_list);
+    var docList = [];
+    UserModel.userDocObj.radar_list.forEach((id) => {
+        UserModel.getUserDoc(id).then((doc) => {
+            docList.push(doc);
+        });
+    });
+    console.log(docList);
+    return docList;
+    // console.log(UserModel.userDocObj.radar_list)
+    // return UserModel.userDocObj.radar_list;
 }
--- src/view/RadarView.js
+++ src/view/RadarView.js
@@ -1,9 +1,51 @@
 import React from "react";
 import {StyleSheet, View, Text, Image} from 'react-native';
+import {getRadarList} from './../controller/RadarController';
 
 export const RadarView = () => {
+    const [radarList, updateRadarList] = React.useState(getRadarList());
+
+    const RadarElement = (props) => {
+        const styles = StyleSheet.create({
+            elementText: {
+                fontSize: 25,
+                fontWeight: 'bold'
+            },
+            elementContainer: {
+                padding: 20,
+                margin: 10,
+                backgroundColor: '#ccc',
+                borderRadius: 10
+            },
+            elementSubtext: {
+                fontSize: 20,
+                color: '#555'
+            }
+        });
+    
+        return(
+            <View style={styles.elementContainer}>
+                <Text style={styles.elementText}>{props.displayName}</Text>
+                <Text style={styles.elementSubtext}>{props.status}</Text>
+            </View>
+        );
+    }
+
+    const renderRadarList = () => {
+        return radarList.map((user) => {
+            return <RadarElement
+                key={user.uId}
+                displayName={user.profile.displayName}
+                status={user.profile.about} />
+        })
+    }
+
     return(
         <>
+        <View style={{margin: 10}}></View>
+            <View>
+                { renderRadarList() }
+            </View>
         </>
     );
 }
