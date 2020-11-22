5ea8c0fbe242c0a5ccdb84f3c7ee07009ad9e763
iangu48 <ian.gu@mail.utoronto.ca>
2020-11-22 16:22:17 -0500

    COD-84 fix ACSModel.js

--- src/model/ACSModel.js
+++ src/model/ACSModel.js
@@ -2,75 +2,50 @@ import {firestore} from '../firebase.js';
 import React from 'react';
 
 export default class ACSModel {
-  acsDocObj = {
-    acsHistory = {};     // object is map of key: value pairs where key is string, value is integer
-  };
-  static acsCollection = firestore().collection('acs');
+    static acsCollection = firestore().collection('acs');
 
-  /**
-   * Adds acsChange value to the current ACS score.
-   * NOTE: does not return ACS Value; use getACSValue to get updated value.
-   *
-   * @params uid           [string]
-   * @params acsChange     [number]
-   *
-   */
-  static addToACS(uid: string, acsChange) {
-    const dateKeyValue = _createKeyValueUsingDate();
-    setACSDocObj(dateKeyValue, acsChange, uid);
-    this.acsCollection.doc(uid).set(this.acsDocObj).then((doc) => {
-        console.log(this.acsDocObj);
-        console.log("Added to ACS!");
-    })
-  }
+    /**
+     * Adds acsChange value to the current ACS score.
+     * NOTE: does not return ACS Value; use getACSValue to get updated value.
+     *
+     * @params uid        string
+     * @params delta      number
+     *
+     */
+    static addToACS(uid: string, delta: number, callback = () => {}) {
+        const key = Date.now()
 
-  /**
-   *  Returns the ACS score of user uid
-   *  Note: ACS cannot be under 100.
-   * @params uid                [string] uid of user
-   * @returns                   [number] acs score
-   */
-  static async getACSValue(uid: string) {
-    const acsDoc = await this.acsCollection.doc(uid).get();
-    const acsMap = acsDoc.data().acsHistory;
-    const result = 100;
-
-    for(const date in acsMap) {
-        result += acsMap[date];
+        this.acsCollection
+            .doc(uid)
+            .update({[key]: delta})
+            .then(callback)
+            .catch(reason => {
+                console.log(reason)
+            })
     }
-    if(result < 100) {
-        result = 100;
+
+    /**
+     *  Returns the ACS score of user uid
+     *  Note: ACS cannot be under 100.
+     */
+    static getACS(uid: string) {
+        return this.acsCollection
+            .doc(uid)
+            .get()
+            .then((ss) => {
+                if (ss.exists) {
+                    return ss.data()
+                } else {
+                    this.createAcsModel(uid).then()
+                }
+
+                return {};
+            })
     }
-    return result;
-  }
 
-
-  /**
-   * Set the ACSDocObj by adding new entry {dateKeyValue: acsChange} to the current
-   * map in firebase
-   *
-   * @params dateKeyValue    [number] date in milliseconds
-   * @params acsChange       [number] delta acs change
-   * @params uid             [string] uid of user updating acs
-   *
-   */
-  static setACSDocObj(dateKeyValue, acsChange, uid) {
-    this.acsCollection
-        .doc(uid)
-        .get()
-        .then(doc => {
-            this.acsDocObj = doc.data();
-            this.acsDocObj.acsHistory[dateKeyValue] = acsChange;
-        }
-  }
-
-   /**
-    *  returns a value representing the current Date in milliseconds
-    *
-    *
-    */
-  static _createKeyValueUsingDate() {
-    const dateAsId = Date.now();
-    return dateAsKey;
-  }
+    static createAcsModel(uid: string) {
+        return this.acsCollection
+            .doc(uid)
+            .set({}, {merge: true})
+    }
 }
