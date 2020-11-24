a7af6fa688eb3fc1b2493417a9858736505355b3
iangu48 <ian.gu@mail.utoronto.ca>
2020-11-24 18:41:56 -0500

    COD-98 add rating indicator for logged-in user responses

--- src/controller/debate/DebateListController.js
+++ src/controller/debate/DebateListController.js
@@ -1,6 +1,6 @@
 import {DebateListView} from "../../view/debate/DebateListView";
 import {AuthContext} from "../../navigation/AuthNavigator";
-import React, {useContext, useEffect, useState} from "react";
+import React, {useContext, useState} from "react";
 import DebateModel from "../../model/DebateModel";
 
 
@@ -8,6 +8,7 @@ export const DebateList = ({navigation}) => {
     const user = useContext(AuthContext)
     const [fetched, setFetched] = useState(false)
     const [list, setList] = useState([])
+    const [personal, setPersonal] = useState()
 
 
     // const d = new Date(Date.now()); TODO use this in prod
@@ -16,19 +17,28 @@ export const DebateList = ({navigation}) => {
     const d = new Date(1606003200000);
 
 
-
-
     if (!fetched) {
-        console.log(d.getTime().toString())
         DebateModel.getAllResponses(d.getTime().toString()).then(docs => {
-            setList(docs)
-            setFetched(true)
-        })
+                setList(docs)
+                DebateModel.getPersonalResponses(user.uid).then(r => {
+                    let p = {}
+                    Object.keys(r).map(k => {
+                        const ratings = Object.values(r[k]['ratings'])
+                        p[k] = ratings.reduce((a, b) => a + b, 0) / ratings.length
+                    })
+
+                    setPersonal(p)
+                    console.log(JSON.stringify(p, null, 2))
+                    setFetched(true)
+                })
+            }
+        )
     }
 
-    function addRating(opUid: string, raterUid: string, rating: number, questionId: string, callback = () => {}) {
+    function addRating(opUid: string, raterUid: string, rating: number, questionId: string, callback = () => {
+    }) {
         DebateModel.addRating(opUid, raterUid, rating, questionId, callback)
     }
 
-    return DebateListView({list, addRating, user});
+    return DebateListView({list, addRating, user, personal});
 }
--- src/view/debate/DebateListView.js
+++ src/view/debate/DebateListView.js
@@ -1,18 +1,19 @@
 import React, {useContext, useEffect, useState} from "react";
 import {View, FlatList, Text} from 'react-native'
-import {Button, List, Modal, Portal, Snackbar} from "react-native-paper";
+import  {Caption,Button, List, Modal, Portal, ProgressBar, Snackbar, Colors} from "react-native-paper";
 import Slider from '@react-native-community/slider';
 
 
 export const DebateListView = (props) => {
-    console.log(JSON.stringify(props.list, null, 2))
-    const [modalVisible, setModalVisible]   = useState(false);
-    const [sbVisible, setSbVisible]   = useState(false);
-    const [uid, setUid]                     = useState('')
-    const [questionId, setQuestionId]       = useState('')
-    const [scale, setScale]                 = useState(0.5)
+    const [modalVisible, setModalVisible] = useState(false);
+    const [sbVisible, setSbVisible] = useState(false);
+    const [uid, setUid] = useState('')
+    const [questionId, setQuestionId] = useState('')
+    const [scale, setScale] = useState(0.5)
+    const [response, setResponse] = useState('')
 
     const showModal = (item) => {
+        setResponse(item.response)
         setUid(item.uid)
         setQuestionId(item.qid)
         setModalVisible(true);
@@ -47,29 +48,58 @@ export const DebateListView = (props) => {
         return <></>;
     };
 
+    function ratingModal() {
+        return (
+            <>
+                <Text> I agree {(scale * 100).toFixed(0)}%</Text>
+                <Slider
+                    style={{width: 200, height: 40}}
+                    minimumValue={0}
+                    maximumValue={1}
+                    value={scale}
+                    step={0.01}
+                    onSlidingComplete={setScale}
+                    minimumTrackTintColor="#FF6060"
+                    maximumTrackTintColor="#000000"
+                />
+                <Button onPress={() => {
+                    setRating();
+                    hideModal();
+                }
+                }>
+                    Submit rating
+                </Button>
+                <Button onPress={hideModal}> Cancel </Button>
+            </>
+        )
+    }
+
+    function viewingModal() {
+        const p = props.personal[questionId]
+        let c = null
+        if (p > 0.7)
+            c = Colors.green600
+        else if (p > 0.35)
+            c = Colors.yellow600
+        else
+            c = Colors.red600
+
+
+        return (
+            <>
+                <Text>Your response received a {p*100}% rating!</Text>
+                <ProgressBar progress={p} color={c} style={{width: 350, marginVertical: 10}}/>
+                <Caption>{response}</Caption>
+                <Button onPress={hideModal}> Close </Button>
+            </>
+        )
+    }
+
     return (
         <View>
             <Portal>
                 <Modal visible={modalVisible} onDismiss={hideModal} contentContainerStyle={containerStyle}>
-                    <Text> I agree {(scale * 100).toFixed(0)}%</Text>
-                    <Slider
-                        style={{width: 200, height: 40}}
-                        minimumValue={0}
-                        maximumValue={1}
-                        value={scale}
-                        step={0.01}
-                        onSlidingComplete={setScale}
-                        minimumTrackTintColor="#FF6060"
-                        maximumTrackTintColor="#000000"
-                    />
-                    <Button onPress={() => {
-                        setRating();
-                        hideModal();
-                    }
-                    }>
-                        Submit rating
-                    </Button>
-                    <Button onPress={hideModal}> Cancel </Button>
+                    {props.user.uid === uid ? viewingModal() : ratingModal()}
                 </Modal>
             </Portal>
 
