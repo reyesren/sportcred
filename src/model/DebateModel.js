97ee58525328b9aa994b86d890f56c17cbd8a32d
iangu48 <ian.gu@mail.utoronto.ca>
2020-11-24 15:17:35 -0500

    COD-98 add jsdoc

--- src/model/DebateModel.js
+++ src/model/DebateModel.js
@@ -9,7 +9,7 @@ export default class DebateModel {
 
     /**
      *
-     * @param date  number  The date in epoch ms at 00:00
+     * @param date  number  The date in epoch ms at 00:00 (12am)
      */
     static getQuestions(date: number) {
         return this.questionsDoc
@@ -19,6 +19,14 @@ export default class DebateModel {
             })
     }
 
+    /**
+     *
+     * @param uid               string
+     * @param questionId        string      (epoch timestamp at 12am)
+     * @param questionString    string      ex: "who is the greatest?"
+     * @param response          string      ex: "kawhi leanard is the greatest!"
+     * @param callback          function
+     */
     static saveResponse(uid: string, questionId: string, questionString: string, response: string, callback = () => {}) {
         this.personalResponseCollection
             .doc(uid)
@@ -40,6 +48,14 @@ export default class DebateModel {
 
     }
 
+    /**
+     *
+     * @param opUid         string
+     * @param raterUid      string
+     * @param rating        number      between 0 and 1 inclusive
+     * @param questionId    string      (epoch timestamp at 12am)
+     * @param callback      function
+     */
     static addRating(opUid: string, raterUid: string, rating: number, questionId: string, callback = () => {}) {
 
         const key = questionId + ".ratings." + raterUid
@@ -52,6 +68,11 @@ export default class DebateModel {
             ).then(callback())
     }
 
+    /**
+     *
+     * @param questionId    string  (epoch timestamp at 12am)
+     * @return {Promise<*>} promise
+     */
     static async getAllResponses(questionId: string) {
         const snapshot = await this.allResponsesCollection.collection(questionId).get()
         return snapshot.docs.map(doc => {
