78a0ed4a79d3213ee7a7b14f8fb2daa1729d1899
Kevin Oh <kevoh1516@gmail.com>
2020-11-26 18:02:42 -0500

    [COD-87] implement backend hook for saving results trivia solo

--- src/controller/TriviaResultsController.js
+++ src/controller/TriviaResultsController.js
@@ -1,8 +1,6 @@
 import React, {useContext} from 'react';
 import {AuthContext} from '../navigation/AuthNavigator';
 import TriviaResultView from '../view/trivia/TriviaResultsView';
-import TriviaChallengeModel from '../model/TriviaChallengeModel';
-import UserModel from '../model/UserModel';
 
 const TriviaResultsController = ({route, navigation}) => {
   const user = useContext(AuthContext);
@@ -11,31 +9,6 @@ const TriviaResultsController = ({route, navigation}) => {
   const goToTriviaLanding = () => {
     navigation.navigate('TriviaLandingController');
   };
-
-  {
-    /*
-  const processResults = () => {
-    if (route.params.mode === 'solo') {
-      // todo
-    }
-    if (route.params.mode === 'head') {
-      TriviaChallengeModel.sendChallenge(
-        user.uid,
-        {
-          opAnswers: {},
-          // opDisplayName: UserModel.getUserDoc(user.uid).profile.displayName,
-          opDisplayName: 'test display name',
-          opUid: 'test uid',
-          questions: route.params.questionIds,
-          score: route.params.score,
-        },
-        Date.now().toString(),
-        () => {},
-      );
-    }
-  };
-  */
-  }
 
   return TriviaResultView({
     score: route.params.score,
